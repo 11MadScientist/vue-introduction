e0042b2338040729cf8fa90dae61acadde6f77e9
11MadScientist <felnovert.lee@gmail.com>
2024-06-26 15:51:42 +0800

    Fix spacing

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="container">
-    <h1>Using v-bind and v-on together.</h1>
-    <input type="text" :value="text" @input="onInput">
-    <h3 class="reflect">{{ text }}</h3>
-  </div>
+      <h1>Using v-bind and v-on together.</h1>
+      <input type="text" :value="text" @input="onInput">
+      <h3 class="reflect">{{ text }}</h3>
+    </div>
 
-  <div class="container">
-    <h1>Using v-model.</h1>
-    <input v-model="vmodeltext">
-    <h3 class="reflect">{{ vmodeltext }}</h3>
-  </div>
+    <div class="container">
+      <h1>Using v-model.</h1>
+      <input v-model="vmodeltext">
+      <h3 class="reflect">{{ vmodeltext }}</h3>
+    </div>
   </div>
 </template>
 
